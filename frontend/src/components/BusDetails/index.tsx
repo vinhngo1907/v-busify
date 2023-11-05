ed1c6b8699b8272a5c8531c933bf30eff7239e3d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-05 14:29:01 +0700

    ( *︾▽︾)

--- frontend/src/components/BusDetails/index.tsx
+++ frontend/src/components/BusDetails/index.tsx
@@ -1,19 +1,34 @@
+import React, { useState } from "react";
 import { Box, Button, Drawer, IconButton, Typography, styled, useTheme } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
+import FareBreakDownCard from '../FareBreakdownCard';
+import { BusDetailsType } from "../../types";
 
-
-const BusDetails = ({ from, to, disabled, time }) => {
+const BusDetails = ({ from, to, disabled, time }: BusDetailsType) => {
     const theme = useTheme();
     const navigate = useNavigate();
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+    const [isAddingPassenger, setIsAddingPassenger] = useState(false)
+    const [isToasterActive, setIsToasterActive] = useState(false)
 
     const bookTicketHandler = () => {
 
     }
 
-    const closeDrawer = () => {
+    const drawerstyle = {
+        width: { xs: '100%', sm: '66.6667%' },
+        padding: '2rem',
+    };
+
+    const openDrawer = () => setIsDrawerOpen(true);
+    const closeDrawer = () => setIsDrawerOpen(false);
+    const handleAddPassenger = (event: React.FormEvent) => {
+        event.preventDefault();
+        const input = event.target as HTMLInputElement;
 
     }
 
@@ -38,7 +53,7 @@ const BusDetails = ({ from, to, disabled, time }) => {
         padding: 1rem;
         min-height: 20vh;
     `;
-    
+
     const CrossIcon = styled(CloseIcon)`
         border: 1px solid rgba(0, 0, 0, 0.5);
         border-radius: 50%;
@@ -85,6 +100,57 @@ const BusDetails = ({ from, to, disabled, time }) => {
                     <Details sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
                         {/* <BusDetailsCard /> */}
                     </Details>
+                </Box>
+                <Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography
+                            fontSize={{ xs: '1.5rem', sm: '1.75rem', md: '2rem' }}
+                            variant="h2"
+                            color={theme.palette.secondary.main}
+                        >
+                            Passenger Details
+                        </Typography>
+                        {/* {!isAddingPassenger} */}
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography
+                        variant="h2"
+                        fontSize={{ xs: '1.5rem', sm: '1.75rem', md: '2rem' }}
+                        color={theme.palette.secondary.main}
+                    >
+                        Fare Breakdown
+                    </Typography>
+                    <FareBreakDownCard />
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        alignItems: 'center',
+                        marginTop: '2rem',
+                    }}
+                >
+                    <Button
+                        variant="contained"
+
+                        sx={{
+                            padding: '0.5rem 2rem',
+                            borderRadius: '8px',
+                            '&:hover': {
+                                backgroundColor: theme.palette.primary.main,
+                            },
+                        }}
+                    >
+                        Pay Now
+                    </Button>
+                    <Toaster />
                 </Box>
             </Drawer>
         </>
