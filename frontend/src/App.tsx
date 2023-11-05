113faf59bc2f0f390dec2be58d893e14477d5cea
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-05 10:44:31 +0700

    (✿◡‿◡)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { Box, ThemeProvider } from '@mui/material'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -14,28 +12,9 @@ function App() {
 		<ThemeProvider theme={theme}>
 			<Box margin={{ xs: "1rem", md: "3rem 5rem" }}>
 				<Navbar />
+				<h1>Vite + React</h1>
 				<Footer />
 			</Box>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</ThemeProvider>
 	)
 }
