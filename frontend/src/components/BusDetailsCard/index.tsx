3e9489df549bec3eab365bec346cea305dfd1b9a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-05 16:46:58 +0700

    (✿◡‿◡)

---
+++ frontend/src/components/BusDetailsCard/index.tsx
@@ -0,0 +1,18 @@
+import { useTheme, Typography, styled } from "@mui/material";
+
+const Icon = styled('img')`
+    width: 40px;
+    height: 40px;
+    
+    @media (max-width: 600px) {
+        width: 30px;
+        height: 30px;
+    }
+`;
+
+function BusDetailsCard(){
+    const theme = useTheme();
+    
+}
+
+export default BusDetailsCard;
