4e7f931b0043d3f1bf1969ed371c592f6cc50fe4
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-11 20:10:45 +0700

    redis service

--- backend/src/redis/redis.service.ts
+++ backend/src/redis/redis.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import Redis from "ioredis";
+import { AppLoggerService } from "src/common/logger/logger.service";
+import { AppConfigService } from "src/config/app-config.service";
 @Injectable()
 export class RedisService {
     client: Redis
-    // private logger: Logg
+    private logger: AppLoggerService;
+    constructor(private readonly appConfig: AppConfigService) {
+        this.logger = new AppLoggerService(RedisService.name);
+        this.client = new Redis({
+            host: this.appConfig.redisConfig.host,
+            port: this.appConfig.redisConfig.port,
+            db: this.appConfig.redisConfig.db,
+            password: this.appConfig.redisConfig.password
+        });
+
+        this.logger.log(`Redis Connected`);
+    }
+
+    async close() {
+        this.client.quit();
+    }
 }
