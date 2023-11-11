698b2742f4958f99d99ceb02c1c0e8e68f7d9810
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-11 17:18:54 +0700

    global jwt

--- backend/src/globalJwt.module.ts
+++ backend/src/globalJwt.module.ts
@@ -1,40 +1,12 @@
-import {
-    ArgumentsHost,
-    Catch,
-    ExceptionFilter,
-    HttpException,
-    HttpStatus,
-    Logger,
-} from '@nestjs/common';
-
-@Catch()
-export class HttpErrorFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const request = ctx.getRequest();
-        const response = ctx.getResponse();
-        const status = exception.getStatus
-            ? exception.getStatus()
-            : HttpStatus.INTERNAL_SERVER_ERROR;
-        const errorResponse = {
-            code: status,
-            timestamp: new Date().toLocaleDateString(),
-            path: request.url,
-            method: request.method,
-            message:
-                status !== HttpStatus.INTERNAL_SERVER_ERROR
-                    ? exception.message || null
-                    : 'Internal Server Error',
-        };
-        if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
-            console.error(exception)
-        }
-
-        Logger.error(
-            `${request.method}${request.url}`,
-            JSON.stringify(errorResponse),
-            'ExceptionFilter',
-        );
-        response.status(404).json(errorResponse);
-    }
-}
+import { Global, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+@Global()
+@Module({
+    imports: [
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+        }),
+    ],
+    exports: [JwtModule],
+})
+export class GlobalJwtModule { }
