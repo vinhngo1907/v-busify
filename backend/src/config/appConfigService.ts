a3a60ae3a2f993f00e80df8e071af328cbe94650
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-11 17:13:33 +0700

    helpers, redis types

--- backend/src/config/appConfigService.ts
+++ backend/src/config/appConfigService.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RedisConfig } from 'src/redis/redis.types';
+import { configKeys } from './constants';
 
 @Injectable()
 export class AppConfigService {
@@ -7,5 +9,15 @@ export class AppConfigService {
 
     get port(): number {
         return this.configService.get('PORT') || 3333;
+    }
+
+    get redisConfig(): RedisConfig{
+        return {
+            host: this.configService.get(configKeys.REDIS_HOST),
+            port: this.configService.get(configKeys.REDIS_PORT),
+            password: this.configService.get(configKeys.REDIS_PASSWORD),
+            db: this.configService.get(configKeys.REDIS_DB),
+            ex: this.configService.get(configKeys.REDIS_EX),
+        }
     }
 }
