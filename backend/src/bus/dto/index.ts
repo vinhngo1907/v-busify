c2f9ad1802793578592e5e5394c32b643992f1fe
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-11 16:22:42 +0700

    bus dto

--- backend/src/bus/dto/index.ts
+++ backend/src/bus/dto/index.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsNotEmpty, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsString, isString } from "class-validator";
 
 export class BusDTO {
     @IsString()
@@ -17,13 +17,38 @@ export class BusDTO {
     @IsNotEmpty()
     conductorId: string;
 
-    // @IsString()
-    // @IsNotEmpty()
-    // busNumber: string;
+    @IsString()
+    @IsNotEmpty()
+    busNumber: string;
 }
 
 export class BusCreateDTO {
     @IsString()
     @IsNotEmpty({ message: 'Bus number is required' })
     number: string;
+}
+
+export class ScheduleDTO {
+    @IsString()
+    @IsNotEmpty()
+    busNumber: string;
+
+    @IsString()
+    @IsNotEmpty()
+    from: string;
+    to: string;
+    departureTime: string;
+    days: [];
+    checkpoints: [];
+    ticketPrice: number
+}
+
+export class ContractorDTO {
+    @IsString()
+    @IsNotEmpty({ message: "name field is required!" })
+    name: string;
+
+    @IsString()
+    @IsNotEmpty({ message: "phone field is required!" })
+    phone: string;
 }
