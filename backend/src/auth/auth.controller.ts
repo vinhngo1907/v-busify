a2b03547f687b6cc4d49392e5948d81e9cb2ed62
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-19 15:29:48 +0700

    🐰

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,30 +1,16 @@
-import { Controller, Get, Inject, Query, UseGuards, OnModuleInit } from '@nestjs/common';
+import { Controller, Get, Inject, Query, UseGuards, OnModuleInit, Post, Res, Body } from '@nestjs/common';
 import { pipe, switchMap, of } from 'rxjs';
 // import { AuthGuard } from 'src/redis/redis.module';
-import { ClientProxy, EventPattern } from '@nestjs/microservices';
-import { DatabaseService } from 'src/database/database.service';
-import { HttpService } from '@nestjs/axios';
-import { Logger } from 'kafkajs';
+// eslint-disable-next-line node/no-extraneous-import
+import {Request, Response} from 'express';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private userService: AuthService,
-        // @Inject('CLIENT_SERVICE') private readonly client: ClientProxy
-    ) { }
-    
-    @Get()
-    // @UseGuards(new AuthGuard())
-    showAllUser(
-        @Query('page') page: number = 1,
-        @Query('limit') limit: number = 10,
-        @Query('status') status: string = undefined,
-        @Query('order_by') order_by: string = 'desc',
-    ) {
-        try {
-           return [];
-        } catch (err: any) {
-            throw err;
-        }
+    constructor(private authService: AuthService,) { }
+
+    @Post('google')
+    async googleAuthRedirect(@Body('code') code: string, @Res() res: Response) {
+        return await this.authService.googlLoginCallback(code, res);
     }
 }
