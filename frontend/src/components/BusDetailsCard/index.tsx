9a347ddf390f57d13db01f67474e1fdadd6b0546
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-12-03 21:27:59 +0700

    �❤

--- frontend/src/components/BusDetailsCard/index.tsx
+++ frontend/src/components/BusDetailsCard/index.tsx
@@ -1,4 +1,8 @@
-import { useTheme, Typography, styled } from "@mui/material";
+import { useTheme, Typography, styled, Box } from '@mui/material';
+import { useOrderStore } from '../../store/orderStore';
+import busIcon from '../../assets/bus-icon.svg';
+import arrorIcon from '../../assets/arrowIcon.svg';
+import scheduleIcon from '../../assets/schedule-icon.svg';
 
 const Icon = styled('img')`
     width: 40px;
@@ -10,9 +14,47 @@ const Icon = styled('img')`
     }
 `;
 
-function BusDetailsCard(){
+function BusDetailsCard() {
     const theme = useTheme();
-    return (<></>);
+    const time = useOrderStore(state => state.time);
+    const source = useOrderStore(state => state.source);
+    const destination = useOrderStore(state => state.destination);
+    return (<>
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: { xs: '10px', md: '2rem' },
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: { xs: '5px', md: '10px' },
+                }}
+            >
+                <Icon src={busIcon} alt='Bus Icon' />
+                <Box>
+                    <Typography
+                        variant="h6"
+                        fontSize={{ xs: '0.8rem', md: '1rem' }}
+                        color={theme.palette.common.black}
+                    >
+                        From
+                    </Typography>
+                    <Typography
+                        variant="h4"
+                        fontSize={{ xs: '1.25rem', md: '1.5rem' }}
+                        color={theme.palette.secondary.main}
+                    >
+                        {source}
+                    </Typography>
+                </Box>
+            </Box>
+            <img src={arrorIcon} alt="Arrow Icon" />
+        </Box>
+    </>);
 }
 
 export default BusDetailsCard;
